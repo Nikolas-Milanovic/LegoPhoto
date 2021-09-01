dc59ff55b00924eb6c0558d306d05d7e9a6b4e64
nikolas milanovic <nikolas.milanovic@uwaterloo.ca>
2021-09-01 17:52:40 -0400

    updated js

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
+// Plan 
+// 1) get the color of the pixels 
+// 2) next, get the average color 
+// 3) thrid, get the Lego color thats closest to the avrg color
+// 4) display the the lego 1 by 1 platte in the browser
+// 5) provide instructions
+
 document.getElementById("input_img").onload = function() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  var img = document.getElementById("input_img");
-  img.crossOrigin = "Anonymous";
-  ctx.drawImage(img, 0, 0);
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255 - imgData.data[i];
-    imgData.data[i+1] = 255 - imgData.data[i+1];
-    imgData.data[i+2] = 255 - imgData.data[i+2];
-    imgData.data[i+3] = 255;
-  }
-  ctx.putImageData(imgData, 0, 0);
-};
-
-// // Plan 
-// // 1) get the color of the pixels 
-// // 2) next, get the average color 
-// // 3) thrid, get the Lego color thats closest to the avrg color
-// // 4) display the the lego 1 by 1 platte in the browser
-// // 5) provide instructions
-
-// document.getElementById("input_img").onload = function() {
-//     var c = document.getElementById("myCanvas");
-//     var ctx = c.getContext("2d");
-//     var img = document.getElementById("input_img");
-//     ctx.drawImage(img, 0, 0);
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("input_img");
+    ctx.drawImage(img, 0, 0);
     
-//     var rgb = {r:0,g:0,b:0};
-//     //var scale=1;
-//     var width = c.width;
-//     var height =  c.length;
-//     var imgData=ctx.getImageData(0, 0, c.width, c.height);
+    var rgb = {r:0,g:0,b:0};
+    //var scale=1;
+    var width = c.width;
+    var height =  c.length;
+    var imgData=ctx.getImageData(0, 0, c.width, c.height);
     
-//     var rgb_pixels = new  Array(length);
+    var rgb_pixels = new  Array(length);
     
-//     //create 2D array [length][width]
-//     //(0,0)............(0,width)
-//     // .                 .
-//     // .                 .
-//     // .                 .
-//     //(length,0).......(length,width)
-//     for(var i=0; i < rgb_pixels.length; i++){
-//         rgb_pixels[i] = new Array(width);
-//     }
+    //create 2D array [length][width]
+    //(0,0)............(0,width)
+    // .                 .
+    // .                 .
+    // .                 .
+    //(length,0).......(length,width)
+    for(var i=0; i < rgb_pixels.length; i++){
+        rgb_pixels[i] = new Array(width);
+    }
 
-//     //get rgb from imgData object and input it into a user friendly 2D array
-//     var i=0;
-//     for(var x=0; x < width; x++){
-//         for(var y=0; y < width; y++){
-//             rgb.r = imgData.data[i];
-//             rgb.b = imgData.data[i+1];
-//             rgb.g = imgData.data[i+2];
-//             rgb_pixels [x][y]=rgb;
-//             i+=4;
-//         }
-//     }
+    //get rgb from imgData object and input it into a user friendly 2D array
+    var i=0;
+    for(var x=0; x < width; x++){
+        for(var y=0; y < width; y++){
+            rgb.r = imgData.data[i];
+            rgb.b = imgData.data[i+1];
+            rgb.g = imgData.data[i+2];
+            rgb_pixels [x][y]=rgb;
+            i+=4;
+        }
+    }
       
-//     console.log(rgb_pixels[0][0]);
-//     //ctx.putImageData(imgData, 0, 0);
+    console.log(rgb_pixels[0][0]);
+    //ctx.putImageData(imgData, 0, 0);
 
 
-//     // rgb.r = ~~(rgb.r/count);
-//     // rgb.g = ~~(rgb.g/count);
-//     // rgb.b = ~~(rgb.b/count);
-//   };
+    // rgb.r = ~~(rgb.r/count);
+    // rgb.g = ~~(rgb.g/count);
+    // rgb.b = ~~(rgb.b/count);
+  };
 
 
 
