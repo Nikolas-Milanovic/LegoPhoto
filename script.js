84ad61124bf6eeb0803648a8627e2c3d45d3819c
nikolas milanovic <nikolas.milanovic@uwaterloo.ca>
2021-09-03 01:46:37 -0400

    added functionality to upload files from local users local computer

--- script.js
+++ script.js
@@ -2,93 +2,88 @@
 // 1) get the color of the pixels DONE
 // 2) next, get the average color DONE
 // 3) thrid, get the Lego color thats closest to the avrg color 
-// 4) display the the lego 1 by 1 platte in the browser
+// 4) display the the lego 1 by 1 platte in the browser DONE
 // 5) provide instructions
 
-document.getElementById("input_img").onload = function() {
+//document.getElementById("input_img").onload = function() {
+  var loadFile = function(event) {
     var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("input_img");
-    ctx.drawImage(img,0,0);
-    var imgData=ctx.getImageData(0, 0, c.width, c.height);
-
-    var scale=40;
-    var height = img.naturalHeight || img.offsetHeight || img.height;
-    var width = img.naturalWidth || img.offsetWidth || img.width;
     
-    var rgb_pixels = new Array(height);
-    
-    //create 2D array [height][width]
-    for(var i=0; i < rgb_pixels.length; i++){
-        rgb_pixels[i] = new Array(width);
-    }
+    var img= document.getElementById("input_img");
+	  img.src = URL.createObjectURL(event.target.files[0]);
 
-    //get rgb from imgData object and input it into a user friendly 2D array
-    var i=0;
-    for(var x=0; x < height; x++){
-        for(var y=0; y < width; y++){
-            //console.log(imgData.data[i]+" "+imgData.data[i+1]+" "+imgData.data[i+2]);
-            var rgb = {r:0,g:0,b:0};
-            rgb.r = imgData.data[i];
-            rgb.g = imgData.data[i+1];
-            rgb.b = imgData.data[i+2];
-            rgb_pixels[x][y]=rgb;
-            i+=4;
+    img.onload = function() {
+
+      var height = img.naturalHeight || img.offsetHeight || img.height;
+      var width = img.naturalWidth || img.offsetWidth || img.width;
+      
+      
+      var ctx = c.getContext("2d");
+      ctx.drawImage(img,0,0);
+      var imgData=ctx.getImageData(0, 0, width, height);
+
+      var scale=40; //Scale represents how many pixels from the inputed_img will represent 1by1 lego plate. 
+      //Example: scale = 10; Then scale*scale=10*10=100 pixels will be represented by the color of one 1by1 lego plate
+
+      //create 2D array [height][width]
+      var rgb_pixels = new Array(height);
+      for(var i=0; i < rgb_pixels.length; i++){
+          rgb_pixels[i] = new Array(width);
+      }
+
+      //Get rgb from imgData object and input it into a user friendly 2D array
+      var i=0;
+      for(var x=0; x < height; x++){
+          for(var y=0; y < width; y++){
+              var rgb = {r:0,g:0,b:0};
+              rgb.r = imgData.data[i];
+              rgb.g = imgData.data[i+1];
+              rgb.b = imgData.data[i+2];
+              rgb_pixels[x][y]=rgb;
+              i+=4;
+          }
+      }
+      
+      //Height and Width must be rounded down appropiatelly to avoid out of bounds expection when calling the getAvrg() func
+      height=scale*(~~(height/scale));
+      width=scale*(~~(width/scale));
+      c.width=width;
+      c.height=height;
+
+      console.log("h"+height);
+      console.log("w"+width);
+
+      //Paint the background of the canvas white
+      // ctx.fillStyle = "#FFFFFF";
+      // ctx.fillRect(0,0,height,width);
+      // ctx.fill();
+      ctx.beginPath();
+      ctx.fillStyle = "white";
+      ctx.fillRect(0,0,width,height);
+
+      //Get the average color of the pixel blocks (sclae*scale) and paint that color onto the canvas
+      for(var row=0; row < height; row+=scale){
+        for(var col=0; col < width; col+=scale){
+
+          var rgb=getAvrg(scale,row,col,rgb_pixels);
+          var col_hex_string=RGBToHex(rgb.r,rgb.g,rgb.b);
+          
+          //ctx.fillStyle = "#FFFFFF";
+          //ctx.fillRect(col,row,scale,scale);
+
+          ctx.beginPath();
+          ctx.fillStyle = col_hex_string;
+          ctx.arc(col+(scale/2),row+(scale/2), scale/2, 0, 2 * Math.PI);
+          ctx.fill();
+
         }
-    }
-
-    height=scale*(~~(height/scale));
-    width=scale*(~~(width/scale));
-
-    console.log(height+" "+width);
-    console.log(rgb_pixels.length);
-    console.log(rgb_pixels[rgb_pixels.length-1].length);
-    console.log(rgb_pixels[height-1][width-1]);
-
-
-    //get the average color of the pixels and paint that color onto the canvas
-    for(var row=0; row < height; row+=scale){
-      for(var col=0; col < width; col+=scale){
-        //console.log("row:"+row+"col:"+col);
-        var rgb=getAvrg(scale,row,col,rgb_pixels);
-        var col_hex_string=RGBToHex(rgb.r,rgb.g,rgb.b);
-        
-       
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(col,row,scale,scale);
-
-        ctx.beginPath();
-        ctx.fillStyle = col_hex_string;
-        ctx.arc(col+(scale/2),row+(scale/2), scale/2, 0, 2 * Math.PI);
-        ctx.fill();
-        //ctx.stroke();
-
-
       }
     }
-
-
-    console.log("i value: "+i);
-    console.log("testing output");
-    console.log(rgb_pixels[500][500]);
-    console.log(getAvrg(scale,500,500,rgb_pixels));
-    console.log(rgb_pixels[0][0]);
-
-
-    //ctx.fillStyle = "rgb(0,0,0)";
-    //ctx.fillStyle = "#FF0000";
-    ctx.fillStyle = "green";
-
-    //ctx.fillStyle="rgba(254,0,0,0.5)";
-
-    //ctx.fillRect(100, 0, scale, scale);
-
-    //ctx.putImageData(imgData, 0, 0);
-
-
   };
+  
 
-
+  //getAvrg(scale,x,y,rgb_pixels) uses the rgb data stored in the rgb_pixels 2D array. To then get the average color of the pixels
+  // in the area as defined by: x,y being the top left corner and scale is the width and height
   function getAvrg(scale,x,y,rgb_pixels){
     var rgb = {r:0,g:0,b:0};
     
@@ -108,6 +103,9 @@ document.getElementById("input_img").onload = function() {
     return rgb;
   }
 
+
+  //RGBToHex(r,g,b) takes the RGB and outputs the HEX in a string format
+  //r,g,b are integer values [0,255]
   function RGBToHex(r,g,b) {
     r = r.toString(16);
     g = g.toString(16);
@@ -127,3 +125,7 @@ document.getElementById("input_img").onload = function() {
 
 
 
+  // console.log(height+" "+width);
+  // console.log(rgb_pixels.length);
+  // console.log(rgb_pixels[rgb_pixels.length-1].length);
+  // console.log(rgb_pixels[height-1][width-1]);
