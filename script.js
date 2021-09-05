5dbe5bc3a19f2dd68b859b4f9c070fa0e88e543a
nikolas milanovic <nikolas.milanovic@uwaterloo.ca>
2021-09-05 02:54:24 -0400

    WORKING. added dropdown menu to select lego colors user wants to use

--- script.js
+++ script.js
@@ -8,15 +8,20 @@
 //document.getElementById("input_img").onload = function() {
 var c = document.createElement("canvas");
 
+
   function display_lego_photo(){
       
       console.log("generate_button clicked");
+      try {
+        var img= document.getElementById("dragged_img");
+        var height = img.naturalHeight || img.offsetHeight || img.height;
+        var width = img.naturalWidth || img.offsetWidth || img.width;
+      } catch (error) {
+        console.log("catch");
+        alert("No Image Inputed.\nPlease Upload Image file");
+        return;
+      }
  
-      var img= document.getElementById("dragged_img");
-
-      var height = img.naturalHeight || img.offsetHeight || img.height;
-      var width = img.naturalWidth || img.offsetWidth || img.width;
-
       c.width=width;
       c.height=height;
   
@@ -73,16 +78,17 @@ var c = document.createElement("canvas");
       ctx.fillRect(0,0,width,height);
 
       //Get the average color of the pixel blocks (sclae*scale) and paint that color onto the canvas
+      var selected_colors=getColorsSelected();
       for(var row=0; row < height; row+=scale){
         for(var col=0; col < width; col+=scale){
 
-          //var rgb=getAvrg(scale,row,col,rgb_pixels);
           var rgb=getAvrg(scale,row,col,rgb_pixels);
+          
+          var lego_color=closest_lego_color(rgb,rgb_pixels,selected_colors);
+          rgb=lego_color.rgb;
           var col_hex_string=RGBToHex(rgb.r,rgb.g,rgb.b);
           
-          //ctx.fillStyle = "#FFFFFF";
-          //ctx.fillRect(col,row,scale,scale);
-
+  
           ctx.beginPath();
           ctx.fillStyle = col_hex_string;
           ctx.arc(col+(scale/2),row+(scale/2), scale/2, 0, 2 * Math.PI);
@@ -135,6 +141,150 @@ var c = document.createElement("canvas");
   
     return "#" + r + g + b;
   }
+
+  /*
+  Lego exact colors:
+  Black
+  Brick Yellow
+  Bright Blue
+  Bright Green
+  Bright Orange
+  Bright Red
+  Bright Yellow
+  Bright Yellowish Green
+  Dark Green
+  Dark Stone Grey
+  Light Purple
+  Medium Lilac
+  Medium Nougat
+  Medium Stone Grey
+  Reddish Brown
+  Silver Metallic
+  White
+*/
+const legocolors = [
+  {
+    "rgb": {r:19,g:19,b:19},
+    "color": "Black",
+  },
+  {
+    "rgb": {r:221,g:196,b:142},
+    "color": "Brick Yellow",
+  },
+  {
+    "rgb": {r:0,g:108,b:183},
+    "color": "Bright Blue",
+  },
+  {
+    "rgb": {r:150,g:199,b:83},
+    "color": "Bright Green",
+  },
+  {
+    "rgb": {r:245,g:136,b:48},
+    "color": "Bright Orange",
+  },
+  {
+    "rgb": {r:221,g:26,b:33},
+    "color": "Bright Red",  /*6*/
+  },
+  {
+    "rgb": {r:225,g:205,b:3},
+    "color": "Bright Yellow",
+  },
+  {
+    "rgb": {r:154,g:202,b:60},
+    "color": "Bright Yellowish Green",
+  },
+  {
+    "rgb": {r:0,g:146,b:71},
+    "color": "Dark Green",
+  },
+  {
+    "rgb": {r:100,g:103,b:101},
+    "color": "Dark Stone Grey",
+  },
+  {
+    "rgb": {r:246,g:173,b:205},
+    "color": "Light Purple",
+  },
+  {
+    "rgb": {r:76,g:47,b:146},
+    "color": "Medium Lilac",
+  },
+  {
+    "rgb": {r:175,g:116,b:70},
+    "color": "Medium Nougat",
+  },
+  {
+    "rgb": {r:160,g:161,b:159},
+    "color": "Medium Stone Grey",
+  },
+  {
+    "rgb": {r:105,g:46,b:20},
+    "color": "Reddish Brown",
+  },
+  {
+    "rgb": {r:135,g:141,b:143},
+    "color": "Silver Metallic",
+  },
+  {
+    "rgb": {r:244,g:244,b:244},
+    "color": "White",
+  },
+]
+
+//getColorsSelected() returns the colors selected in the drop down
+function getColorsSelected(){
+  var len=legocolors.length;
+  console.log(len);
+  var selected=[];
+  for(var i=0;i<len;i++){
+    var id=""+i;
+    console.log("id"+id);
+    if(document.getElementById(id).checked){
+      selected.push(i);
+    }
+  }
+  console.log(selected);
+  return selected;
+}
+
+//closest_lego_color(input ,rgb_pixels) compares the rgb paramter input to the
+// array of legocolors, and returns the closest color
+//O(n)
+function closest_lego_color(input ,rgb_pixels, selected_colors){
+  var closest_color=legocolors[0];
+  var closest_distance=3*Math.pow(255,2);//max distance
+
+  var len=selected_colors.length;
+  for(var i=0;i<len;i++){
+    var index=selected_colors[i];
+    var d=Math.pow(input.r-legocolors[index].rgb.r,2)
+    +Math.pow(input.g-legocolors[index].rgb.g,2)
+    +Math.pow(input.b-legocolors[index].rgb.b,2);
+    if(d<closest_distance){
+      closest_distance=d;
+      closest_color=legocolors[i];
+    }
+  }
+  return closest_color;
+}
+
+
+//Checkbox functionality
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
 
 
 
